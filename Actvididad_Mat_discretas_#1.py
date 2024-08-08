0bccf2cfca17c34fe454bb9eece5d4ec71289930
Kofi <107807110+Kof27@users.noreply.github.com>
2024-08-08 10:24:39 -0500

    First Commit

---
+++ Actvididad_Mat_discretas_#1.py
@@ -0,0 +1,9 @@
+from base32 import *
+bit_exp = []
+
+number_to_bin = base32Binary(10.8,2)
+number = number_to_bin.number
+i= number
+print(number)
+First_bit = number_to_bin.positive_or_negative(number)
+number_to_bin.number_to_binary(First_bit,10.8,2,bit_exp,i,number_to_bin.exp,number_to_bin.exp_string)
---
+++ base32.py
@@ -0,0 +1,61 @@
+class base32Binary():
+    def __init__(self,number:float,base:int):
+        self.number = number
+        self.base = base
+        self.exp = 0
+        self.bit_exp = []
+        self.exp_string = ''
+        self.x = str
+        i = number
+
+        
+    def positive_or_negative(self,number):
+        bit_positive_or_negative = 0
+        if number > 1:
+            bit_positive_or_negative = 0
+        elif number < 0:
+            bit_positive_or_negative = 1
+        return bit_positive_or_negative
+    
+    def number_to_binary(self,negative_or_positive,number,base,bit_exp,i,exp,exp_string):
+        while int(i) != 0:
+            i /= base
+            exp += 1
+            print (i)
+            exp = exp 
+            print (exp)
+    
+        #exponent
+        while exp > 0:
+            binary_value = int(exp % 2)
+            exp = int(exp / 2)
+            bit_exp.append(binary_value)
+            bit_exp.reverse()
+        for x in bit_exp:
+            #print (x, end='')
+            exp_string += str(x)
+            exp_string = exp_string.zfill(8)
+    
+        print('')
+        #mantisa
+        mantisa_binary = ''
+        mantisa = number - int(number)
+    
+        #in case mantisa is negative to prevent 000000000000 
+        if mantisa < -0:
+            mantisa_positive = mantisa * -1
+        else:
+            mantisa_positive = mantisa
+        
+        print(mantisa)
+        precision = 23
+        while (precision):
+            mantisa_positive*=2
+            bit_mantisa = int(mantisa_positive)
+            if (bit_mantisa == 1) :   
+                mantisa_positive -= bit_mantisa  
+                mantisa_binary += '1'
+            else : 
+                mantisa_binary += '0'
+            precision -= 1
+        print(negative_or_positive,'|',exp_string,'|',mantisa_binary)
